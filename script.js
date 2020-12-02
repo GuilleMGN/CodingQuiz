ce8ba2b1ec1e1e8ba90730c9e6694e61a7f020e6
MGuillen <matthewguillen777@gmail.com>
2020-12-01 22:23:13 -0500

    added files and worked on main htmlcss

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// I need to create 10 questions in 75 seconds
+
+// var secondsDisplay = document.createElement("p");
+
+
+
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
